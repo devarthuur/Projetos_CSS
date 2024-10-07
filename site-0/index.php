d1b29c09eee1f75d5a3539a6044bcabcbb5d0b62
Arthuuralb <arthur.alb7858@gmail.com>
2024-10-07 10:24:46 -0300

    projetos CSS

---
+++ site-0/index.php
@@ -0,0 +1,6 @@
+<?php 
+      $hoje = date('Y-m-d');
+      if ($hoje == date('Y').'-09-13'){
+           echo ':) FELIZ DIA DAS MULHRES!';
+      } else { 
+           echo ':( MAIS UM DIA CODANDO'; } ?>
